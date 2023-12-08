197e4d87b956d50e2a7125fdf68e408e2c3575cd
Heelsie <andyheels@gmail.com>
2023-12-08 15:17:35 +0000

    repo update

--- Encoder.py
+++ Encoder.py
@@ -1,6 +1,6 @@
 import string
 alphabet = list(string.ascii_uppercase)
-
+# Hi Joseph, congratuations on making it this far! Now you need to use this code to decode your present.
 
 def encode(text):
     result = ''
@@ -15,5 +15,8 @@ text = input("Your message: ")
 #print(text)    
 
 
-print("Your encoded message is: ", encode(text))
-#print("Your decoded message is: ", encode(encode(text)))
+print("Encoded message is: ", encode(text))
+#print("Decoded message: ", encode(encode(text)))
+
+
+# Bonus code IEU8C-WATRK-53OEW
